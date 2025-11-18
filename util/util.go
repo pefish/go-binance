76269d6ed6e2799d442fe672eddf87757f76f7d9
pefish <pefish@pefishs-MacBook-Pro.local>
2025-11-18 15:34:31 +0800

    v1.10.16

--- util/util.go
+++ util/util.go
@@ -9,9 +9,10 @@ import (
 )
 
 type CirculatingSupplyInfoType struct {
-	UnlockedAmount  float64 `json:"unlockedAmount"`
-	LockedAmount    float64 `json:"lockedAmount"`
-	UntrackedAmount float64 `json:"untrackedAmount"`
+	UnlockedAmount  float64 `json:"unlockedAmount"`  // 流通数量
+	LockedAmount    float64 `json:"lockedAmount"`    // 锁定数量
+	UntrackedAmount float64 `json:"untrackedAmount"` // 未跟踪数量
+	TotalAmount     float64 `json:"totalAmount"`     // 总量
 }
 
 func GetCirculatingSupplyInfo(logger i_logger.ILogger, currency string) (*CirculatingSupplyInfoType, error) {
@@ -19,7 +20,11 @@ func GetCirculatingSupplyInfo(logger i_logger.ILogger, currency string) (*Circul
 		Code    string `json:"code"`
 		Message string `json:"message"`
 		Data    struct {
-			Progress CirculatingSupplyInfoType `json:"progress"`
+			Progress struct {
+				UnlockedAmount  float64 `json:"unlockedAmount"`
+				LockedAmount    float64 `json:"lockedAmount"`
+				UntrackedAmount float64 `json:"untrackedAmount"`
+			} `json:"progress"`
 		} `json:"data"`
 	}
 	_, _, err := go_http.NewHttpRequester(go_http.WithLogger(logger), go_http.WithTimeout(10*time.Second)).GetForStruct(
@@ -37,5 +42,10 @@ func GetCirculatingSupplyInfo(logger i_logger.ILogger, currency string) (*Circul
 	if httpResult.Code != "000000" {
 		return nil, fmt.Errorf("GetCirculatingSupplyInfo error. code: %s, message: %s", httpResult.Code, httpResult.Message)
 	}
-	return &httpResult.Data.Progress, nil
+	return &CirculatingSupplyInfoType{
+		UnlockedAmount:  httpResult.Data.Progress.UnlockedAmount,
+		LockedAmount:    httpResult.Data.Progress.LockedAmount,
+		UntrackedAmount: httpResult.Data.Progress.UntrackedAmount,
+		TotalAmount:     httpResult.Data.Progress.UnlockedAmount + httpResult.Data.Progress.LockedAmount + httpResult.Data.Progress.UntrackedAmount,
+	}, nil
 }
